7181cda6d4989834b532d69ff6558b3bbdaa7a2a
Joe OHeron <joeoheron@gmail.com>
2022-09-22 19:09:50 -0500

    site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  siteMetadata: {
+    // Used for the site title and SEO
+    title: `Joe O'Heron`,
+    // Used for SEO
+    description: `This is Joe O'Heron's personal website`,
+    // Used for resolving images in social cards
+    siteUrl: `https://joeoheron.com`,
+    // Used for SEO in social cards
+  },
   plugins: [{
     resolve: `@react95/gatsby-theme`,
     options: {
